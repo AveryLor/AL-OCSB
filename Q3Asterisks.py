a8d044ed7493c22208e322b233c876d140e78fa5
ALor <avery.lor@stu.ocsb.ca>
2022-05-31 16:21:53 -0400

    jhk

--- Q3Asterisks.py
+++ Q3Asterisks.py
@@ -1,6 +1,21 @@
-addingAsterix = "*"
+# NAME OF AUTHOR: Avery Lor
+# NAME OF THE PROGRAM: Q3Asterisks.py
+# DATE OF CREATION: 2022-05-31
+# PURPOSE OF PROGRAM: To print 25 rows with 80 asteriks on each row, however , each row must be printed at a time.
 
-for i in range(1):
-    addingAsterix *= 80
-    for i in range(25):
-        print(addingAsterix)
+# VARIABLE DEFINITION
+
+addingAsteriks = 0 # This variable here is needed to add 80 asteriks to.
+
+# INPUT
+
+# PROCESSING
+addingAsteriks = "*"
+
+for i in range(25): # Running itA 25 times for all the columns
+    for i in range(1):
+        addingAsteriks *= 80 # Adding the 80 asteriks
+
+# OUTPUT
+        for i in range(1):
+                print(addingAsteriks) #Printing 1 row
